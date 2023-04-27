748d867d4e74a2eea06fc637fb8a6ba2043d811d
mbr1337 <mb651424@gmail.com>
2023-04-27 18:01:59 +0200

    lab8 first angular project + typescript

--- app/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  port: process.env.PORT || 3000,
-  // databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://mb:ODc0RcJv5oKCMwKH@taw.i3umimd.mongodb.net/test',
-  JwtSecret: process.env.JWT_SECRET || 'secret'
-};
-
-export default config;
