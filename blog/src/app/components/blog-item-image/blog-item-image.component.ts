75d48b8792fd050ce4b89da985d2d757b76dc254
mbr1337 <mb651424@gmail.com>
2023-05-16 20:35:34 +0200

    lab9

---
+++ blog/src/app/components/blog-item-image/blog-item-image.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'blog-item-image',
+  templateUrl: './blog-item-image.component.html',
+  styleUrls: ['./blog-item-image.component.css'],
+})
+export class BlogItemImageComponent {
+  @Input() image?: string;
+}
