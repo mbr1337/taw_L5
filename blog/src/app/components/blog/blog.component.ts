1b6634f14b30fadc074603042371bacd62c9704b
mbr1337 <mb651424@gmail.com>
2023-05-27 19:16:56 +0200

    event bubbling fix + pipe(tap())

--- blog/src/app/components/blog/blog.component.ts
+++ blog/src/app/components/blog/blog.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css'],
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent implements OnInit, OnDestroy {
   @Input() filterText: string = '';
   public items$: any;
+  private postDeletedSubscription?: Subscription;
+  private blogRefreshedSubscription?: Subscription;
   constructor(private service: DataService) {}
   ngOnInit() {
     this.getAll();
-    this.service.postDeleted.subscribe((postId: number) => {
-      console.log('Post deleted:', postId);
-      this.items$ = this.items$.filter((item: any) => item.id !== postId);
-      // Refresh the component after post deletion
-      // this.refreshComponent();
-    });
+    this.postDeletedSubscription = this.service.postDeleted.subscribe(
+      (postId: number) => {
+        console.log('Post deleted:', postId);
+        this.items$ = this.items$.filter((item: any) => item.id !== postId);
+      }
+    );
+    this.blogRefreshedSubscription = this.service.blogRefreshed.subscribe(
+      () => {
+        this.getAll();
+      }
+    );
   }
   getAll() {
     this.service.getAll().subscribe((response) => {
@@ -26,5 +33,13 @@ export class BlogComponent implements OnInit {
   }
   refreshComponent() {
     this.getAll();
+  }
+  ngOnDestroy() {
+    if (this.postDeletedSubscription) {
+      this.postDeletedSubscription.unsubscribe();
+    }
+    if (this.blogRefreshedSubscription) {
+      this.blogRefreshedSubscription.unsubscribe();
+    }
   }
 }
--- blog/src/app/services/data.service.ts
+++ blog/src/app/services/data.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   private url = 'http://localhost:3001';
   constructor(private http: HttpClient) {}
+  public blogRefreshed: EventEmitter<number> = new EventEmitter<number>();
   postDeleted = new EventEmitter<number>();
   getAll() {
     return this.http.get(this.url + '/api/posts');
@@ -17,8 +19,16 @@ export class DataService {
     console.log(postData);
     return this.http.post(this.url + '/api/addPost', postData);
   }
+  // Metoda .pipe() w Angular służy do łączenia wielu operatorów razem w celu utworzenia łańcucha operacji na obserwowalnym strumieniu.
+  // Każdy operator w łańcuchu wykonuje określone zadanie lub transformację danych emitowanych przez obiekt observable.
+  // tutaj .pipe() służy do łączenia operatorów w łańcuchy i wykonywania dodatkowych akcji po wysłaniu żądania HTTP DELETE.
+  // Operator tap() umożliwia wykonywanie efektów ubocznych lub działań na emitowanych wartościach z observable bez modyfikowania samych wartości.
   removePost(id: number) {
-    this.postDeleted.emit(id);
-    return this.http.delete(this.url + '/api/removePost/' + id);
+    return this.http.delete(this.url + '/api/removePost/' + id).pipe(
+      tap(() => {
+        this.postDeleted.emit(id);
+        this.blogRefreshed.emit();
+      })
+    );
   }
 }
