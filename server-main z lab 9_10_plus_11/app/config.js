5591f6831abc8676ec3f0d8701c5a875e378bc04
mbr1337 <mb651424@gmail.com>
2023-05-28 10:44:03 +0200

    refactor

--- server-main z lab 9_10_plus_11/app/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  port: process.env.PORT || 3001,
-  databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://mb:leakedpass@weatherverifiabilityapp.5fwsjmi.mongodb.net/',
-  JwtSecret: process.env.JWT_SECRET || 'secret'
-};
-
-export default config;
