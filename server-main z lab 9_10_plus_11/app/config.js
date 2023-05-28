074b1a589e4d4dbde3f841ecb79f46257dc3915c
mbr1337 <119813565+mbr1337@users.noreply.github.com>
2023-05-28 10:43:14 +0200

    Update config.js

--- server-main z lab 9_10_plus_11/app/config.js
+++ server-main z lab 9_10_plus_11/app/config.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3001,
-  databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://mb:leakedpass@weatherverifiabilityapp.5fwsjmi.mongodb.net/',
+  databaseUrl: process.env.MONGODB_URI,
   JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 
